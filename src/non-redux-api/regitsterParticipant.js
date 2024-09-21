2d33b936ecba2890b051a709937bc3f6b6760e29
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-21 20:53:10 +0300

    remove comments

--- src/non-redux-api/regitsterParticipant.js
+++ src/non-redux-api/regitsterParticipant.js
@@ -5,7 +5,7 @@ axios.defaults.baseURL = "https://events-service-ff1w.onrender.com";
 export const registerParticipant = async (participantData) => {
   const { fullName, eventId, heardFrom, birthDate, email } = participantData;
   await axios.post(
-    `/register/${eventId}`,  // Include eventId in the URL path
-    { fullName, heardFrom, birthDate, email }  // Pass other data in the request body
+    `/register/${eventId}`,  
+    { fullName, heardFrom, birthDate, email }
   );
 };
