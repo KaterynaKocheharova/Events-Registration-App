5a8430d1c8b1085bc5a301272794f91ccab37745
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-21 17:33:57 +0300

    asdF

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -8,6 +8,7 @@ import {
   FormErrorMessage,
   Box,
   Heading,
+  Flex,
 } from "@chakra-ui/react";
 import * as Yup from "yup";
 
@@ -32,6 +33,7 @@ const registerParticipantSchema = Yup.object().shape({
 
 const RegistrationForm = () => {
   const fullNameId = useId();
+  const emailId = useId();
 
   return (
     <Box p="1rem" boxShadow="base" width="400px">
@@ -52,56 +54,90 @@ const RegistrationForm = () => {
             <Heading as="h3" size="md" mb="2rem">
               Register for the event
             </Heading>
-            {/* FULL NAME */}
-            <Field name="fullName">
-              {({ field }) => (
-                <FormControl isInvalid={errors.fullName && touched.fullName}>
-                  <FormLabel htmlFor={fullNameId}>Full Name</FormLabel>
-                  <Input id={fullNameId} {...field} />
-                  <FormErrorMessage>{errors.fullName}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            {/* EMAIL */}
-            <Field name="email">
-              {({ field }) => (
-                <FormControl isInvalid={errors.email && touched.email}>
-                  <FormLabel>Email</FormLabel>
-                  <Input type="email" {...field} />
-                  <FormErrorMessage>{errors.email}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            {/* BIRTH DATE */}
-            <Field name="birthDate">
-              {({ field }) => (
-                <FormControl isInvalid={errors.birthDate && touched.birthDate}>
-                  <FormLabel>Birth Date</FormLabel>
-                  <Input type="date" {...field} />
-                  <FormErrorMessage>{errors.birthDate}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            {/* HEARD FROM */}
-            <Field name="heardFrom">
-              {({ field }) => (
-                <FormControl isInvalid={errors.heardFrom && touched.heardFrom}>
-                  <FormLabel>Heard From</FormLabel>
-                  <Box>
-                    {["social media", "friends", "found myself"].map((option) => (
-                      <FormControl key={option} display="inline-block" mr="1rem">
-                        <Field type="radio" name="heardFrom" value={option} />
-                        <FormLabel as="span">{option}</FormLabel>
-                      </FormControl>
-                    ))}
-                  </Box>
-                  <FormErrorMessage>{errors.heardFrom}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Button type="submit" colorScheme="purple" mt="1rem">
-              Submit
-            </Button>
+            <Flex direction="column" gap="1rem">
+              {/* =================================================== FULL NAME ================================================== */}
+              <Field name="fullName">
+                {({ field }) => (
+                  <FormControl isInvalid={errors.fullName && touched.fullName}>
+                    <FormLabel htmlFor={fullNameId} color="purple.400">
+                      Full Name
+                    </FormLabel>
+                    <Input
+                      id={fullNameId}
+                      {...field}
+                      focusBorderColor="purple.400" // Add this line
+                    />
+                    <FormErrorMessage>{errors.fullName}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+
+              {/* ==================================================== EMAIL ============================================ */}
+              <Field name="email">
+                {({ field }) => (
+                  <FormControl isInvalid={errors.email && touched.email}>
+                    <FormLabel color="purple.400">Email</FormLabel>
+                    <Input
+                      id={emailId}
+                      type="email"
+                      {...field}
+                      focusBorderColor="purple.400"
+                    />
+                    <FormErrorMessage>{errors.email}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              {/* ================================================== BIRTH DATE ========================================== */}
+              {/* BIRTH DATE */}
+              <Field name="birthDate">
+                {({ field }) => (
+                  <FormControl
+                    isInvalid={errors.birthDate && touched.birthDate}
+                  >
+                    <FormLabel>Birth Date</FormLabel>
+                    <Input
+                      type="date"
+                      {...field}
+                      focusBorderColor="purple.400"
+                    />
+                    <FormErrorMessage>{errors.birthDate}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              {/* =================================================== HEARD FROM ========================================== */}
+              <Field name="heardFrom">
+                {({ field }) => (
+                  <FormControl
+                    isInvalid={errors.heardFrom && touched.heardFrom}
+                  >
+                    <FormLabel>Heard From</FormLabel>
+                    <Box>
+                      {["social media", "friends", "found myself"].map(
+                        (option) => (
+                          <FormControl
+                            key={option}
+                            display="inline-block"
+                            mr="1rem"
+                          >
+                            <Field
+                              type="radio"
+                              name="heardFrom"
+                              value={option}
+                            />
+                            <FormLabel as="span">{option}</FormLabel>
+                          </FormControl>
+                        )
+                      )}
+                    </Box>
+                    <FormErrorMessage>{errors.heardFrom}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+
+              <Button type="submit" colorScheme="purple" mt="1rem">
+                Submit
+              </Button>
+            </Flex>
           </Form>
         )}
       </Formik>
