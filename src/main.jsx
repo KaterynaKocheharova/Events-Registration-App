c7b968a76e5de2172d22d4661eaab14a8304a125
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-22 17:49:52 +0300

    clean main.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-
 import { Provider } from "react-redux";
-import { Toaster } from "react-hot-toast";
 import { store, persistor } from "./redux/store.js";
-
-import "modern-normalize";
-import App from "./App.jsx";
-import "./index.css";
 import { PersistGate } from "redux-persist/es/integration/react";
+import App from "./App.jsx";
+import "modern-normalize";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter>
-        <ChakraProvider>
-          <App />
-          <Toaster />
-        </ChakraProvider>
-      </BrowserRouter>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
