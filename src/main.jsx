08d3707da8deda6b1b1047d56d614c1f9c7d1d53
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-12 00:00:42 +0300

    fix blank page error - I had Persistor using non-existent value

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store.js";
-import { PersistGate } from "redux-persist/es/integration/react";
+import { store } from "./redux/store.js";
 import App from "./App.jsx";
 import "modern-normalize";
 import "./index.css";
@@ -12,13 +11,11 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <ChakraProvider>
-            <App />
-          </ChakraProvider>
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {
   persistStore,
-  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -9,18 +8,12 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { eventsReducer } from "../redux/events/slice";
 
-const eventsPersistConfiguration = {
-  key: "events",
-  storage,
-  whitelist: ["currentPage"],
-};
 
 export const store = configureStore({
   reducer: {
-    events: persistReducer(eventsPersistConfiguration, eventsReducer),
+    events: eventsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
